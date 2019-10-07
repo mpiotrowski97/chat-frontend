71a6bfb4707bc682c6bab8e8daa33108a277f5dd
Michał Piotrowski <97.piotrowski.michal@gmail.com>
2019-10-07 21:51:35 +0200

    Add the channels sidebar

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,25 @@
         <a href="#" class="px-3 text-white">Logout</a>
       </div>
     </div>
+
+    <div class="mt-4 flex flex-row">
+      <div class="w-1/6 px-6">
+        <h3 class="text-gray-600 font-bold text-3xl">
+          Channels
+        </h3>
+        <hr>
+        <div class="border-0 rounded">
+          <ul class="border-2 rounded mt-3">
+            <li class="p-6 border-b-2 bg-blue-600 text-white"># General</li>
+            <li class="p-6 border-b-2"># Weapons</li>
+            <li class="p-6"># Combat</li>
+          </ul>
+        </div>
+      </div>
+      <div class="w-4/6">
+      </div>
+      <div class="w-1/6">3</div>
+    </div>
   </div>
 </template>
 
