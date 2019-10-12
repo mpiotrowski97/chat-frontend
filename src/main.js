f184e5e26d430fe568be5fbfa14f44d492b63021
Michał Piotrowski <97.piotrowski.michal@gmail.com>
2019-10-12 22:00:08 +0200

    Add validation for the register form

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
+import { ValidationProvider, ValidationObserver, extend } from "vee-validate";
+import {confirmed, email, required} from "vee-validate/dist/rules";
 
 import '@/assets/css/tailwind.css'
 
+extend('required', {
+  ...required,
+  message: 'This field is required'
+});
+
+extend('email', {
+  ...email,
+  message: 'Please enter a valid email'
+});
+
+extend('confirmed', {
+  ...confirmed,
+  message: 'Please confirm password'
+});
+
 Vue.config.productionTip = false;
+
+Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver);
 
 new Vue({
   render: h => h(App),
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,45 +1,70 @@
 <template>
     <div class="flex items-center justify-center h-full w-full bg-blue-400 text-gray-700">
         <div class="w-2/6">
-            <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
-                <div class="mb-4">
-                    <label class="block text-grey-darker text-sm font-bold mb-2" for="name">
-                        Name
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="name"
-                           type="text" placeholder="Name" v-model="user.name">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-grey-darker text-sm font-bold mb-2" for="email">
-                        Email
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="email"
-                           type="text" placeholder="Email" v-model="user.email">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
-                        Password
-                    </label>
-                    <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-grey-darker mb-3"
-                           id="password" type="password" placeholder="******************" v-model="user.password">
-                    <p class="text-red-500 text-xs italic">Please choose a password.</p>
-                </div>
-                <div class="mb-6">
-                    <label class="block text-grey-darker text-sm font-bold mb-2" for="password_confirmation">
-                        Confirm password
-                    </label>
-                    <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-grey-darker mb-3"
-                           id="password_confirmation" type="password" placeholder="******************"
-                           v-model="user.passwordConfirmation">
-                    <p class="text-red-500 text-xs italic">Please choose a password.</p>
-                </div>
-                <div class="flex items-center justify-between">
-                    <button class="bg-blue-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="button">
-                        Sign Up
-                    </button>
-                    <router-link :to="{name: 'login'}" class="text-blue-400 font-bold">Sign in</router-link>
-                </div>
-            </div>
+            <validation-observer v-slot="{ invalid }">
+                <form>
+                    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+                        <div class="mb-4">
+                            <validation-provider rules="required" v-slot="{ errors }">
+                                <label class="block text-grey-darker text-sm font-bold mb-2" for="name">
+                                    Name
+                                </label>
+                                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                                       :class="{ 'border-red-500': errors[0] }"
+                                       id="name"
+                                       type="text" placeholder="Name" v-model="user.name">
+                                <p class="text-red-500 text-xs italic pt-3" v-if="errors[0]">{{ errors[0] }}</p>
+                            </validation-provider>
+                        </div>
+                        <div class="mb-4">
+                            <validation-provider rules="required|email" v-slot="{ errors }">
+                                <label class="block text-grey-darker text-sm font-bold mb-2" for="email">
+                                    Email
+                                </label>
+                                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                                       :class="{ 'border-red-500': errors[0] }"
+                                       id="email"
+                                       type="text" placeholder="Email" v-model="user.email">
+                                <p class="text-red-500 text-xs italic pt-3" v-if="errors[0]">{{ errors[0] }}</p>
+                            </validation-provider>
+                        </div>
+                        <div class="mb-4">
+                            <validation-provider rules="required" ref="password" v-slot="{ errors }" vid="password">
+                                <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+                                    Password
+                                </label>
+                                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3"
+                                       :class="{ 'border-red-500': errors[0] }"
+                                       id="password" type="password" placeholder="******************"
+                                       v-model="user.password">
+                                <p class="text-red-500 text-xs italic pt-3" v-if="errors[0]">{{ errors[0] }}</p>
+                            </validation-provider>
+                        </div>
+                        <div class="mb-6">
+                            <validation-provider rules="required|confirmed:password" v-slot="{ errors }">
+                                <label class="block text-grey-darker text-sm font-bold mb-2"
+                                       for="password_confirmation">
+                                    Confirm password
+                                </label>
+                                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3"
+                                       :class="{ 'border-red-500': errors[0] }"
+                                       id="password_confirmation" type="password" placeholder="******************"
+                                       v-model="user.passwordConfirmation">
+                                <p class="text-red-500 text-xs italic pt-3" v-if="errors[0]">{{ errors[0] }}</p>
+                            </validation-provider>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <button class="bg-blue-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
+                                    :class="{ 'disabled': invalid }"
+                                    type="button"
+                                    :disabled="invalid">
+                                Sign Up
+                            </button>
+                            <router-link :to="{name: 'login'}" class="text-blue-400 font-bold">Sign in</router-link>
+                        </div>
+                    </div>
+                </form>
+            </validation-observer>
         </div>
     </div>
 </template>
@@ -61,4 +86,7 @@
 </script>
 
 <style scoped>
+    .disabled {
+        @apply .bg-blue-300 .cursor-not-allowed
+    }
 </style>
